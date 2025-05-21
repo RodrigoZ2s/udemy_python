038bdbcf90139f7ba5d0da6d10305ca8ae989029
Rodrigo Z2s <rodrigoppm2601@gmail.com>
2025-05-20 22:39:12 -0300

    Subindo aulas

--- Python Intermediário/aula86.py
+++ Python Intermediário/aula86.py
@@ -10,6 +10,18 @@ dc = {
     if isinstance(valor, str) else valor
     for chave, valor 
     in produto.items()
+    if chave != "categoria"
 }
 
-print(dc)
+lista = [
+    ('a', 'valor a'),
+    ('b', 'valor a'),
+    ('b', 'valor a'),
+]
+dc = {
+    chave: valor
+    for chave, valor in lista
+}
+
+s1 = {2 ** i for i in range(10)}
+print(s1)
