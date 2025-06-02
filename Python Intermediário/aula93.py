795fe1467405c1cb4e71635aa85c97e144f7ca19
Rodrigo Z2s <rodrigoppm2601@gmail.com>
2025-06-02 19:40:48 -0300

    Subindo aulas

--- Python Intermediário/aula93.py
+++ Python Intermediário/aula93.py
@@ -14,8 +14,10 @@ except ZeroDivisionError:
     print('Dividiu por zero.')
 except NameError:
     print('Nome b não está definido')
-except (TypeError, IndexError):
+except (TypeError, IndexError) as error:
     print('TypeError + IndexError')
+    print("MSG:", error)
+    print("Nome:", error.__class__.__name__)
 except Exception:
     print('ERRO DESCONHECIDO.')
 
